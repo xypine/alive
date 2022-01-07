baa8ddac78d314812d3b743f4848cc7f2894860e
Elias Eskelinen <elias.eskelinen@pm.me>
2022-01-08 00:37:31 +0200

    init

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def helloWorld():
+    return {'msg':'Hello World!'}
+
+@app.route("/alive")
+def alive():
+    return {'alive':'true'}
+
+@app.route("/time")
+def givetime():
+    return {'time':datetime.utcnow()}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
